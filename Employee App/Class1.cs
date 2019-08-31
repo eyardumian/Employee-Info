31f0ebda599881d3f1e443ca4f59ca1454ba0429
Erika Yardumian <eyardumian@hotmail.com>
2019-08-30 21:54:42 -0700

    Created employee class and fields

---
+++ Employee App/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_App
+{
+    class Employee
+    {
+        //Field
+        private string _name;
+        private int _idNumber;
+        private string _department;
+        private string _position;
+
+        
+    }
+}
